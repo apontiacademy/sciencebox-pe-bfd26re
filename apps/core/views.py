ce970983bd02814540d2a655bacc1c2107ff113b
karolcavalcante33-afk <karolcavalcante33@gmail.com>
2026-05-22 14:15:04 -0300

    Ajustes visuais na seção Sobre Nós

---
+++ apps/core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404
+from apps.products.models import Produto
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def produto_detalhe(request, id):
+    produto = get_object_or_404(Produto, id=id)
+
+    return render(request, 'produto_detalhe.html', {
+        'produto': produto
+    })
+
+
+def kits(request):
+    produtos = Produto.objects.all()
+
+    return render(request, 'kits.html', {
+        'produtos': produtos
+    })
+
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+
+def solucoes(request):
+    return render(request, 'solucoes.html')
+
+
+def contato(request):
+    return render(request, 'contato.html')
